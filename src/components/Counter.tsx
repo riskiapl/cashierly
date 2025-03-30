db8ee7f70e61c6d94d6be4427eb41f6aa22f5c01
Riski Agung <riski97@gmail.com>
2025-03-30 15:38:27 +0700

    add axios

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,6 +5,7 @@ export default function Counter() {
   const [count, setCount] = createSignal(0);
 
   createEffect(() => {
+    console.log(`Count: ${count()}`);
     if (count() % 2 === 0) {
       Alert.success(`You clicked ${count()} times!`);
     } else {
